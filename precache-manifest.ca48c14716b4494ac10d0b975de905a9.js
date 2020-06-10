3bcf67e5a160d5cea18fabafb6ac9f0c23f2dae3
Tanushree Unakal <tanu.unakal@berkeley.edu>
2020-06-10 12:30:52 +0400

    Updates

---
+++ precache-manifest.ca48c14716b4494ac10d0b975de905a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3484a9250b61f9051719e7464a3b5ae",
+    "url": "/profile4friends/index.html"
+  },
+  {
+    "revision": "77c313501488f4ea3972",
+    "url": "/profile4friends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "67d8b76c6c40be451629",
+    "url": "/profile4friends/static/css/main.a3218a25.chunk.css"
+  },
+  {
+    "revision": "77c313501488f4ea3972",
+    "url": "/profile4friends/static/js/2.fa470885.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/profile4friends/static/js/2.fa470885.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67d8b76c6c40be451629",
+    "url": "/profile4friends/static/js/main.191c8e58.chunk.js"
+  },
+  {
+    "revision": "4573d6287ed3fcea1da0",
+    "url": "/profile4friends/static/js/runtime-main.3bb77381.js"
+  }
+]);
